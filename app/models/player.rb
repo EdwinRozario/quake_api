2e9eb20814cf85de61ee05bb7a0cf99a78d4f62d
edwinrozario <rozarioed@gmail.com>
2016-07-16 16:37:32 +1200

    Added relations between models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ApplicationRecord
+	belongs_to :game
+
+	has_many :kills, :class_name => 'Kill', :foreign_key => 'killer_id'
+  has_many :deaths, :class_name => 'Kill', :foreign_key => 'victim_id'
 end

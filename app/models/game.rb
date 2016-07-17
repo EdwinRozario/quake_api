d058a54785a9d132c67e1750efec8a668c632714
edwinrozario <rozarioed@gmail.com>
2016-07-17 16:11:31 +1200

    Fixing tabs to spaces

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,10 @@
 class Game < ApplicationRecord
-	has_many :game_players
-	has_many :kills
+  has_many :game_players
+  has_many :kills
 
-	has_many :players, through: :game_players
+  has_many :players, through: :game_players
 
-	def real_players
-		self.players.where.not(name: '<world>')
-	end
+  def real_players
+    self.players.where.not(name: '<world>')
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,8 @@
 class Player < ApplicationRecord
-	has_many :game_players
+  has_many :game_players
 
-	has_many :kills, :class_name => 'Kill', :foreign_key => 'killer_id'
-	has_many :deaths, :class_name => 'Kill', :foreign_key => 'victim_id'
+  has_many :kills, :class_name => 'Kill', :foreign_key => 'killer_id'
+  has_many :deaths, :class_name => 'Kill', :foreign_key => 'victim_id'
 
-	has_many :games, through: :game_players
+  has_many :games, through: :game_players
 end
